551714894fd7485aa607372160dba99fe05372d9
aniellonigrocogliano <aniello.nigro.cogliano@gmail.com>
2024-03-27 10:34:21 +0100

    fix: correzione

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const btn = document.getElementById("btn"); // object | null
-
+let userAge = 0;
+let userKm = 0;
 btn.addEventListener("click", function () {
     //Raccolta dati
     const systemKM = 0.21; //number
@@ -11,42 +12,53 @@ btn.addEventListener("click", function () {
     console.log(ageImput);
     const age = ageImput.value; // string
     console.log(age);
+           let userAge = parseInt(age);// number
+       let userKm = parseInt(km);// number
+    if (isNaN(userAge) || isNaN(userKm)) {
+        alert("Non hai inserito tutti i dati necessari per il calcolo.");
 
-    userAge = parseInt(age);// number
-    userKm = parseInt(km);// number
-
-    //calcolo prezzo)
-    let userPrize = userKm * systemKM; //number
-    let userDiscount = 0; //number
-    let prize = userPrize;
-    let userDiscountNum = 0;
-
-    if (userAge < 18) {
-        userDiscount = userPrize * 0.20;
-        userDiscountNum = 20;
-    } else if (userAge > 65) {
-        userDiscount = userPrize * 0.40;
-        userDiscountNum = 40;
-    } else {
-        userDiscount = 0;
+    }else if  (userAge<1 || userKm <1) {
+        alert("Non puoi inserire valori negativi.");
     }
+    else {
 
-    userPrize= userPrize-userDiscount;
-    userPrize=userPrize*100;
-    userPrize=Math.round(userPrize);
-    userPrize=userPrize/100;
-    userDiscount=userDiscount*100;
-    userDiscount=Math.round(userDiscount);
-    userDiscount=userDiscount/100;
-    userDiscount=0-userDiscount;
 
-    //verifica dati
-     console.log(userKm, typeof userKm,);
+        //calcolo prezzo)
+        let userPrize = userKm * systemKM; //number
+        let userDiscount = 0; //number
+        let prize = userPrize;
+        let userDiscountNum = 0;
 
-    //stampa risultato
-    document.getElementById("userkm").innerHTML = userKm;
-    document.getElementById("prize").innerHTML = prize;
-    document.getElementById("discountnumber").innerHTML = userDiscountNum;
-   document.getElementById("discountprize").innerHTML = userDiscount;
-    document.getElementById("userprize").innerHTML = userPrize;
-})
+        if (userAge < 18) {
+            userDiscount = userPrize * 0.20;
+            userDiscountNum = 20;
+        } else if (userAge > 65) {
+            userDiscount = userPrize * 0.40;
+            userDiscountNum = 40;
+        } else {
+            userDiscount = 0;
+        }
+
+        userPrize = userPrize - userDiscount;
+        userPrize = userPrize * 100;
+        userPrize = Math.round(userPrize);
+        userPrize = userPrize / 100;
+        userDiscount = userDiscount * 100;
+        userDiscount = Math.round(userDiscount);
+        userDiscount = userDiscount / 100;
+        userDiscount = 0 - userDiscount;
+        prize = prize * 100;
+        prize = Math.round(prize);
+        prize = prize / 100;
+
+        //verifica dati
+        console.log(userKm, typeof userKm,);
+
+        //stampa risultato
+        document.getElementById("userkm").innerHTML = userKm;
+        document.getElementById("prize").innerHTML = prize;
+        document.getElementById("discountnumber").innerHTML = userDiscountNum;
+        document.getElementById("discountprize").innerHTML = userDiscount;
+        document.getElementById("userprize").innerHTML = userPrize;
+   
+} })
